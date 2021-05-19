9b1e5136f5f88870b59dee7483741db7b0ed4e91
liyinglilyyang <liying415@yahoo.com>
2021-05-19 10:40:26 -0700

    k

--- 499/src/total.java
+++ 499/src/total.java
@@ -1,7 +1,8 @@
 public class total {
 
+	
    public static void main(String[] args) {
-        
+      //Set up two numbers and also with the total
       int n1 = 100, n2 = 200, total;
       total = n1 + n2;
 
